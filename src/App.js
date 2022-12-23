75e339ba8d47c02f4c267c45da99dad2a5d93670
Kefas Gisung <gisungkefas@gmail.com>
2022-12-23 14:00:38 +0100

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// import React, { useState } from 'react'
+// import './App.css';
+//
+// export default function App(){
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div className='d-flex justify-content-start alugn-items-center' style={{height: "100vh"}}>
+//     <div>
+//         <p>{count}</p>
+//         <div className='d-flex'>
+//             <button className='btn btn-success me-3'>Increment</button>
+//             <button className='btn btn-danger ms-3'>Decrement</button>
+//         </div>
+//     </div>
+// </div>
+//   );
+// }
+//
+// export default App;
